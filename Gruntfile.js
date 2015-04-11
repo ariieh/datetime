9bc684e1ad09586bf783c9e835a30c5937f3938f
Arieh Smith <arieh.smith@gmail.com>
2015-04-11 18:46:15 -0400

    Add Gruntfile with useful neo4j tasks

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    shell: {
+      startNeo4j: {
+        command: 'db/neo4j-community-2.2.0/bin/neo4j start'
+      },
+      stopNeo4j: {
+        command: 'db/neo4j-community-2.2.0/bin/neo4j stop'
+      },
+      restartNeo4j: {
+        command: 'db/neo4j-community-2.2.0/bin/neo4j restart'
+      },
+      clearDB: {
+        command: 'rm -rf db/neo4j-community-2.2.0/data/graph.db/'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-shell');
+
+  // Neo4j
+  grunt.registerTask('startneo', ['shell:startNeo4j']);
+  grunt.registerTask('stopneo', ['shell:stopNeo4j']);
+  grunt.registerTask('restartneo', ['shell:restartNeo4j']);
+  grunt.registerTask('clrdb', ['shell:stopNeo4j', 'shell:clearDB', 'shell:startNeo4j']);
+};
