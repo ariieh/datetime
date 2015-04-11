c4c6f84f97ce6e12a01ba124675eab0667ce2e69
Arieh Smith <arieh.smith@gmail.com>
2015-04-11 10:59:49 -0400

    Add body parsing

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var express = require('express');
 var app = express();
 var routes = require('./routes');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 // Express config
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
 app.post('/users', routes.users.create);
