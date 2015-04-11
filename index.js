56a1f4123d2cea0c08224c671fd04f5e79da90b9
Arieh Smith <arieh.smith@gmail.com>
2015-04-11 10:59:38 -0400

    Add server logging

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var routes = require('./routes');
+var logger = require('morgan');
 
 // Express config
 app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
 
 // Routes
 app.post('/users', routes.users.create);
