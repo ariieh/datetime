f69d1876df9943d39760e2e8b15f3304cfe3a454
Arieh Smith <arieh.smith@gmail.com>
2015-04-18 14:05:43 -0400

    Get FB permissions for everything

--- routes/views.js
+++ routes/views.js
@@ -1,5 +1,22 @@
 exports.home = function(req, res, next) {
   res.render('index', {
-    FB_APP_ID: process.env.FB_APP_ID
+    FB_APP_ID: process.env.FB_APP_ID,
+    FB_PERMISSIONS: [
+      "public_profile",
+      "email",
+      "user_friends",
+      "user_actions.books",
+      "user_actions.fitness",
+      "user_actions.music",
+      "user_actions.news",
+      "user_actions.video",
+      "user_birthday",
+      "user_likes",
+      "user_photos",
+      "user_religion_politics",
+      "user_tagged_places",
+      "user_education_history",
+      "user_groups"
+    ].join(",")
   });
 }
